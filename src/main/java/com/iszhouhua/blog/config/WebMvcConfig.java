93c917ba236942614ccfaadbd7de9fe4696a45f4
zhouhua <iszhouhua@163.com>
2021-08-16 22:06:09 +0800

    默认上传目录中的内容自动加载

--- src/main/java/com/iszhouhua/blog/config/WebMvcConfig.java
+++ src/main/java/com/iszhouhua/blog/config/WebMvcConfig.java
@@ -1,14 +1,16 @@
 package com.iszhouhua.blog.config;
 
+import com.iszhouhua.blog.common.constant.ConfigConst;
+import com.iszhouhua.blog.common.constant.StorageType;
 import com.iszhouhua.blog.common.interceptor.AdminInterceptor;
 import com.iszhouhua.blog.common.interceptor.LoginInterceptor;
 import com.iszhouhua.blog.common.resolver.CustomAugmentResolver;
+import com.iszhouhua.blog.common.storage.StorageConfig;
+import com.iszhouhua.blog.service.ConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 import java.util.List;
 
@@ -72,5 +74,17 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(new CustomAugmentResolver());
+    }
+
+    @Autowired
+    private ConfigService configService;
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        StorageConfig config = configService.getConfigObject(ConfigConst.FILE_STORAGE, StorageConfig.class);
+        if(config.getType()== StorageType.LOCAL){
+            String resourceHandler = "/"+config.getLocalPrefix()+"/**";
+            String resourceLocations = "file:"+config.getLocalDirectory()+"/"+config.getLocalPrefix()+"/";
+            registry.addResourceHandler(resourceHandler.replace("//","/")).addResourceLocations(resourceLocations.replace("//","/"));
+        }
     }
 }
